ffe5c3984733e4ad0a3893772de903a5b7c116fc
Muhammad Haseeb Khalid <m.haseebk54@gmail.com>
2026-02-07 16:46:31 +0500

    initial commit

---
+++ langgraph_backend.py
@@ -0,0 +1,35 @@
+from langgraph.graph import StateGraph,START,END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_ollama import ChatOllama
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+
+class ChatState(TypedDict):
+
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+
+llm = ChatOllama(model="deepseek-v3.1:671b-cloud")
+
+def chat_node(state: ChatState):
+
+  messages = state['messages']
+
+  response = llm.invoke(messages)
+
+  return {'messages':[response]}
+
+
+graph = StateGraph(ChatState)
+
+checkpointer = InMemorySaver()
+
+graph.add_node(chat_node, 'chat_node')
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+
